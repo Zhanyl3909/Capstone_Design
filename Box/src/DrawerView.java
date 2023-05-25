ce45aa0916fd2d10b5fbce70e327d6b7c7c12c00
Dzholchieva Zhanylai <dzh.2002z@gmail.com>
2023-05-25 16:41:28 +0900

    change member Box's member function

--- Box/src/DrawerView.java
+++ Box/src/DrawerView.java
@@ -5,15 +5,17 @@ import java.lang.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 public class DrawerView extends JPanel
     implements MouseListener, MouseMotionListener
 {
 
     int MAX = 100;
-    Box pBox;  //      Box 가리키는 pBox reference
-    Box boxes[] = new Box[MAX];  //every time, created box will store in boxes array
-    int nBox = 0;
+    Box pBox;
+
+    //Arraylist
+    ArrayList<Box> boxes = new ArrayList<Box>();
     DrawerView() {
         pBox = null;
         addMouseListener(this);
@@ -23,13 +25,10 @@ public class DrawerView extends JPanel
     public void paintComponent(Graphics g) {
        super.paintComponent(g);
 
-       for(int i=0; i<nBox; i++) {
+       for(int i=0; i<boxes.size(); i++) {
+           Box p = boxes.get(i);
+           p.draw(g);
 
-           int minX = Math.min(boxes[i].x1, boxes[i].x2);
-           int minY = Math.min(boxes[i].y1,boxes[i].y2);
-           int width = Math.abs(boxes[i].x2-boxes[i].x1);
-           int height = Math.abs(boxes[i].y2-boxes[i].y1);
-           g.drawRect(minX,minY,width,height);
        }
 
      }
@@ -42,25 +41,17 @@ public class DrawerView extends JPanel
     @Override
     public void mousePressed(MouseEvent e) {
         pBox = new Box(e.getX(), e.getY());
-        boxes[nBox++] = pBox;
+        boxes.add(pBox);
+
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
 
-
-      pBox.x2 = e.getX();
-      pBox.y2 = e.getY();
-
-
-        int minX = Math.min(pBox.x1, pBox.x2);
-        int minY = Math.min(pBox.y1,pBox.y2);
-        int width = Math.abs(pBox.x2-pBox.x1);
-        int height = Math.abs(pBox.y2- pBox.y1);
-
         Graphics g = getGraphics();
-        g.drawRect(minX,minY,width,height);
+        pBox.setXY2(e.getX(),e.getY());
+        pBox.draw(g);
 
     }
 
@@ -78,25 +69,9 @@ public class DrawerView extends JPanel
     //Mouse Motion Listener
     @Override
     public void mouseDragged(MouseEvent e) {
-
-       int minX = Math.min(pBox.x1, pBox.x2);
-       int minY = Math.min(pBox.y1, pBox.y2);
-       int width = Math.abs(pBox.x2 - pBox.x1);
-       int height = Math.abs(pBox.y2 - pBox.y1);
-
-       Graphics g = getGraphics();
-       g.setXORMode(getBackground());
-       g.drawRect(minX, minY, width, height);
-
-       pBox.x2 = e.getX();
-       pBox.y2 = e.getY();
-
-       minX = Math.min(pBox.x1, pBox.x2);
-       minY = Math.min(pBox.y1, pBox.y2);
-       width = Math.abs(pBox.x2 - pBox.x1);
-       height = Math.abs(pBox.y2 - pBox.y1);
-       g.drawRect(minX, minY, width, height);
-
+        Graphics g = getGraphics();
+        g.setXORMode(getBackground());
+        pBox.drawing(g,e.getX(),e.getY());
 
     }
 
