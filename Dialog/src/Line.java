f1b15ce012bded3701ba6b6cd4121519c6f83d7d
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-05-28 23:43:58 +0900

    Update Line.java

---
+++ Dialog/src/Line.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Line extends TwoPointFigure {
+
+
+
+    Line(){
+    }
+    Line(int x, int y) {
+        super(x, y);
+    }
+    Line(int x1, int y1, int x2, int y2) {
+        super(x1,y1,x2,y2);
+    }
+
+    void draw(Graphics g) {
+
+        g.drawLine(x1,y1,x2,y2);
+
+    }
+}
