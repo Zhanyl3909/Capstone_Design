f3024dc8bfc5a9b326f48f62f06df06282a9dd55
Zhanylai3909 <dzh.2002z@gmail.com>
2023-06-09 19:31:03 +0900

    add copy function

---
+++ Clone/src/Box.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+
+public class Box extends TwoPointFigure {
+
+    Box(Color color){
+        super(color);
+    }
+    Box(Color color,int x, int y)
+    {
+        super(color,x,y);
+    }
+
+    Box(Color color,int x1, int y1, int x2, int y2)
+    {
+        super(color,x1,y1,x2,y2);
+    }
+
+    void draw(Graphics g) {
+
+        int minX = Math.min(x1,x2);
+        int minY = Math.min(y1, y2);
+        int width = Math.abs(x2-x1);
+        int height = Math.abs(y2-y1);
+        g.drawRect(minX,minY,width,height);
+    }
+    Figure copy() {
+        //upcasting
+        Box newBox = new Box(color,x1,y1,x2,y2);
+        newBox.popup = popup;
+        newBox.move(MOVE_DX,MOVE_DY);
+        return newBox;
+    }
+}
