b9572c223ef0f7ffc573886edff9985892cbef76
Zhanylai3909 <dzh.2002z@gmail.com>
2023-06-29 19:56:32 +0900

    add tv figure

---
+++ TV_Figure/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame drawerFrame = new DrawerFrame();
+        drawerFrame.setVisible(true);
+    }
+}
+
+
+
+
---
+++ TV_Figure/src/MainPopup.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+class MainPopup extends Popup {
+
+
+    MainPopup(DrawerView view) {
+
+        super("figure");
+
+
+        JMenuItem pointItem = new JMenuItem(view.getPointAction());
+        popupPtr.add(pointItem);
+
+        JMenuItem boxItem = new JMenuItem(view.getBoxAction());
+        popupPtr.add(boxItem);
+
+        JMenuItem circleItem = new JMenuItem(view.getCircleAction());
+        popupPtr.add(circleItem);
+
+        JMenuItem lineItem = new JMenuItem(view.getLineAction());
+        popupPtr.add(lineItem);
+
+        JMenuItem tvItem = new JMenuItem(view.getTVAction());
+        popupPtr.add(tvItem);
+    }
+
+}
+
+
